f3d3e0e8003792e5f10e8021b84bbbf2b2cae64a
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-02-16 15:45:07 +0530

    #include<stdio.h> int pr(int a) { 	int i; 	if(a==1) 	{ 		return 0; 	} 	for(i=2;i*i<=a;i++) 	{ 		if(a%i==0) 		{ 			return 0; 		} 	} 	return 1; } int main() {   int a;   scanf("%d",&a);   int b[a],i,t,k;   for(i=0;i<a;i++)   {   	scanf("%d",&b[i]); 	  } 	  for(i=0;i<a;i++) 	  { 	  	t=b[i]; 	  	for(t=b[i];t>0;t--) 	  	{ 	  		if(pr(t)) 	  		{ 	  			//printf("%d",t); 	  			break; 			  } 		  } 		  	for(k=b[i];;k++) 	  	{ 	  		if(pr(k)) 	  		{ 	  			//printf("%d",k); 	  			break; 			  } 		  } 		  if((b[i]-t)<=(k-b[i])) 		  { 		  	printf("%d ",t); 		  } 		  else 		  { 		  	printf("%d ",k); 		  } 	  } }

---
+++ Nearest_Prime.c
@@ -0,0 +1,58 @@
+
+#include<stdio.h>
+int pr(int a)
+{
+	int i;
+	if(a==1)
+	{
+		return 0;
+	}
+	for(i=2;i*i<=a;i++)
+	{
+		if(a%i==0)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+int main()
+{
+  int a;
+  scanf("%d",&a);
+  int b[a],i,t,k;
+  for(i=0;i<a;i++)
+  {
+  	scanf("%d",&b[i]);
+	  }	
+	  for(i=0;i<a;i++)
+	  {
+	  	t=b[i];
+	  	for(t=b[i];t>0;t--)
+	  	{
+	  		if(pr(t))
+	  		{
+	  			//printf("%d",t);
+	  			break;
+			  }
+		  }
+		  	for(k=b[i];;k++)
+	  	{
+	  		if(pr(k))
+	  		{
+	  			//printf("%d",k);
+	  			break;
+			  }
+		  }
+		  if((b[i]-t)<=(k-b[i]))
+		  {
+		  	printf("%d
+",t);
+		  }
+		  else
+		  {
+		  	printf("%d
+",k);
+		  }
+	  }
+}
