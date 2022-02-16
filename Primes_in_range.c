4982b2c2de2e19c575ebb3bc4a02a1d9429041bb
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-02-16 15:27:28 +0530

    #include<stdio.h> int pr(int a) { 	int i; 	if(a==1) 	{ 		return 0; 	} 	for(i=2;i*i<=a;i++) 	{ 		if(a%i==0) 		{ 			return 0; 		} 	} 	return 1; } int main() { 	int a,b,i,c=0; 	scanf("%d%d",&a,&b);//printf("%d",pr(5)); 	for(i=a;i<=b;i++) 	{ 		if(pr(i)) 		{ 			c+=1; 		//	printf("%d",i); 		} 	} 	printf("%d",c); }

---
+++ Primes_in_range.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int pr(int a)
+{
+	int i;
+	if(a==1)
+	{
+		return 0;
+	}
+	for(i=2;i*i<=a;i++)
+	{
+		if(a%i==0)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+int main()
+{
+	int a,b,i,c=0;
+	scanf("%d%d",&a,&b);//printf("%d",pr(5));
+	for(i=a;i<=b;i++)
+	{
+		if(pr(i))
+		{
+			c+=1;
+		//	printf("%d",i);
+		}
+	}
+	printf("%d",c);
+}
