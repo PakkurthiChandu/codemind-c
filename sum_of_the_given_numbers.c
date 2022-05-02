805b184ab8936284e13c5461918e63e726b4faf8
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-05-02 18:37:14 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],b[n],i;     for(i=0;i<n;i++)     {         scanf("%d%d",&a[i],&b[i]);     }     for(i=0;i<n;i++)     printf("%d ",a[i]+b[i]); }

--- sum_of_the_given_numbers.c
+++ sum_of_the_given_numbers.c
@@ -1,20 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	int a,s=0,d,j;
-	scanf("%d",&a);
-	d=2*a;
-	int b[d],i;
-	for(i=0;i<d;i++)
-	{
-		scanf("%d",&b[i]);
-	}
-
-	for(j=0;j<d;j+=2)
-	{
-		s=b[j]+b[j+1];
-	printf("%d
-",s);
-	s=0;
-	}
+    int n;
+    scanf("%d",&n);
+    int a[n],b[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d%d",&a[i],&b[i]);
+    }
+    for(i=0;i<n;i++)
+    printf("%d
+",a[i]+b[i]);
 }
